8adf1e350377788b26a54d0b4826febcb5d0f865
Wesley Alencar <igorusp@gmail.com>
2020-03-25 17:55:47 -0300

    Primeiro Commit

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const routes = express.Router();
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+//LOGIN
+routes.post('/sessions', SessionController.create);
+
+//ONGS
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
+//INCIDENTS
+routes.post('/incidents',IncidentController.create);
+routes.get('/incidents',IncidentController.index);
+routes.delete('/incidents/:id',IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
